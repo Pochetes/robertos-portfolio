ca0bbec8974e220d4f0255c516c32d1a4f2bfa7e
Pochetes <robertomiguel2001@gmail.com>
2022-01-22 01:18:28 -0500

    added base layout (not finished)

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,47 @@
+import * as React from "react"
+import { get } from "theme-ui"
+import { Global } from "@emotion/react"
+
+type LayoutProps = {
+    children: React.ReactNode
+    className?: string
+}
+
+const Layout = ({ children, className = `` }: LayoutProps) => (
+    <React.Fragment>
+        <Global 
+            styles={(t) => ({
+                "*": {
+                    boxSizing: `inherit`,
+                    "&:before": {
+                        boxSizing: `inherit`,
+                    },
+                    "&:after": {
+                        boxSizing: `inherit`,
+                    },
+                },
+                html: {
+                    fontSize: `18px`,
+                    WebkitTextSizeAdjust: `100%`,
+                },
+                img: {
+                    borderStyle: `none`,
+                },
+                pre: {
+                    fontFamily: `monospace`,
+                    fontSize: `1em`,
+                },
+                "[hidden]": {
+                    display: `none`,
+                },
+                "::selection": {
+                    backgroundColor: get(t, `colors.primary`),
+                    color: get(t, `colors.background`),
+                },
+            })}
+        />
+        <main className={className}>{children}</main>
+    </React.Fragment>
+)
+
+export default Layout
