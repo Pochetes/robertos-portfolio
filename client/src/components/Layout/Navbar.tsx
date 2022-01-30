91b0ac0c378861ed465b1f0193cc9db0a8c036d3
Pochetes <robertomiguel2001@gmail.com>
2022-01-30 12:32:42 -0500

    created base navbar component for layout

---
+++ client/src/components/Layout/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Navbar = () => (
+    <nav>
+        <div>
+            <a href="#">
+                <span>Roberto</span>
+            </a>
+            <div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Resume</a></li>
+                    <li><a href="#">Extras</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div>
+                LD-btn
+            </div>
+        </div>
+    </nav>
+);
+
+export default Navbar;
