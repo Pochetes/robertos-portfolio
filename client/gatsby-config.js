2324210acce14471d2e87359ede878ace517c55c
Pochetes <robertomiguel2001@gmail.com>
2022-01-30 12:32:19 -0500

    moved layout component into its own folder

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         defaultLayouts: {
-          default: require.resolve("./src/components/Layout.tsx")
+          default: require.resolve("./src/components/Layout/Layout.tsx")
         }
       }
     }
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/Layout/Layout"
 import Hero from "../components/Hero"
 import About from "../components/About"
 import Projects from "../components/Projects"
