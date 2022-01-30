f13ea28b2b8980b6d9fd7703ac8fe21b6f293d9b
Pochetes <robertomiguel2001@gmail.com>
2022-01-30 17:49:52 -0500

    removed boilerplate images and added my own (not all)

---
+++ client/static/index.js
@@ -0,0 +1,15 @@
+import lightModeImg from './ld-mode/light.png'
+import darkModeImg from './ld-mode/dark.png'
+import github from './social/github.png'
+import linkedin from './social/linkedin.png'
+import twitter from './social/twitter.png'
+import scrollingArrow from './arrow/scrolling-arrow.svg'
+
+export {
+    lightModeImg,
+    darkModeImg,
+    github,
+    linkedin,
+    twitter,
+    scrollingArrow
+}
