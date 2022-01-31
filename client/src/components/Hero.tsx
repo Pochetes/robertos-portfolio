11ef9a637f6fb7765e423980b9826fedd861f5cc
Pochetes <robertomiguel2001@gmail.com>
2022-01-30 20:31:18 -0500

    implemented html structure for hero component (no css)

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,11 +1,22 @@
 import * as React from "react"
 // @ts-ignore
-import About from "../sections/about"
+import Layout from './Layout/Layout'
+import Contacts from "./Layout/Contacts"
 
 const Hero = () => (
-    <div>
-        <h1>Hero Section</h1>
-    </div>
+    <Layout>
+        <section>
+            <div>
+                <div>
+                    <h1>Some Text for Intro</h1>
+                </div>
+                <Contacts />
+                <div>
+                    <img src="https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo="/>
+                </div>
+            </div>
+        </section>
+    </Layout>
 )
 
 export default Hero
