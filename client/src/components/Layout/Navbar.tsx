f99bd0cf9f81fece0261c8650c72e583e4626ed6
Pochetes <robertomiguel2001@gmail.com>
2022-01-30 22:17:27 -0500

    centered navbar to start playing w/ tailwind

--- client/src/components/Layout/Navbar.tsx
+++ client/src/components/Layout/Navbar.tsx
@@ -7,21 +7,21 @@ import {
 
 const Navbar = () => (
     <nav>
-        <div>
-            <a href="#">
+        <div className="flex flex-row flex-auto">
+            <a className="basis-1/3" href="#">
                 <span className="text-left">Roberto</span>
             </a>
-            <div>
-                <ul className="text-center">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Resume</a></li>
-                    <li><a href="#">Extras</a></li>
-                    <li><a href="#">Contact</a></li>
+            <div className="basis-1/3">
+                <ul className="text-center flex flex-row flex-auto">
+                    <li className="basis-1/12"><a href="#">Home</a></li>
+                    <li className="basis-1/12"><a href="#">About</a></li>
+                    <li className="basis-1/12"><a href="#">Projects</a></li>
+                    <li className="basis-1/12"><a href="#">Resume</a></li>
+                    <li className="basis-1/12"><a href="#">Extras</a></li>
+                    <li className="basis-1/12"><a href="#">Contact</a></li>
                 </ul>
             </div>
-            <div>
+            <div className="basis-1/3">
                 <img src={lightModeImg} alt="light mode icon" />
                 <img src={darkModeImg} alt="dark mode icon" />
             </div>
