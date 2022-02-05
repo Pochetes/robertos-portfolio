9ca1f55bc1c7f52971c83b8a07b27d79bb12223f
Pochetes <robertomiguel2001@gmail.com>
2022-02-05 18:24:51 -0500

    started about component (structuring but no styling)

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -3,14 +3,22 @@ import Layout from "../Layout/Layout"
 
 const About = () => (
     <section>
-        <div>
-            <img src="" alt="picture of interest 1" />
-            <img src="" alt="picture of interest 2" />
-            <img src="" alt="picture of interest 3" />
-            <img src="" alt="picture of interest 4" />
-        </div>
-        <div>
-            <div>Some text about me!</div>
+        <div className="min-h-screen mx-auto">
+            <div className="min-h-screen flex flex-row">
+                <div className="min-w-1/2">
+                    <div className="w-16 h-16 bg-white rounded-full"></div>
+                    <div className="w-16 h-16 bg-white rounded-full"></div>
+                    <div className="w-16 h-16 bg-white rounded-full"></div>
+                    <div className="w-16 h-16 bg-white rounded-full"></div>
+                </div>
+                <div className="min-w-1/2 flex flex-col justify-center items-center">
+                    <div className="p-10">
+                        <div>
+                            <h1 className="font-extrabold leading-tight font-3xl text-8xl">I love to build.</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 )
