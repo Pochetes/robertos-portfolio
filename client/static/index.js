3b1559d059027f91fb14c6de93daa989b4b2b5cc
Pochetes <robertomiguel2001@gmail.com>
2022-02-05 18:20:58 -0500

    added profile pic for hero section

--- client/static/index.js
+++ client/static/index.js
@@ -2,8 +2,10 @@ import github from './social/github.svg'
 import linkedin from './social/linkedin.svg'
 import twitter from './social/twitter.svg'
 import scrollingArrow from './arrow/scrolling-arrow.svg'
+import pfp from './pfp.jpeg'
 
 export {
+    pfp,
     github,
     linkedin,
     twitter,
