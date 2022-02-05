3e180902908a10fd75208a8e09b2e3c8e7836dcd
Pochetes <robertomiguel2001@gmail.com>
2022-02-05 18:23:12 -0500

    updated Hero section (w/o animations)

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,26 +1,29 @@
 import * as React from "react"
 // @ts-ignore
 import Contacts from "./Contacts"
+import { pfp } from "../../../static/"
 
 const Hero = () => (
     <section>
         <div className="min-h-screen mx-auto">
             <div className="min-h-screen flex flex-row">
                 <div className="min-w-1/2 flex flex-col justify-center items-center">
-                    <div className="p-10">
-                        <div>
-                            <h1 className="font-extrabold leading-tight font-3xl text-8xl">Hi! 👋</h1>
-                            <h1 className="mt-4 font-extrabold leading-tight font-3xl text-7xl">
-                                I'm
-                                <span className="text-transparent bg-gradient-to-r bg-clip-text from-blue-500 to-green-500"> Roberto</span>
-                                .
-                            </h1>
-                        </div>
+                    <div className="max-w-3xl mx-auto text-center">
+                        <h1 className="text-7xl font-extrabold sm:text-6xl"
+                        >
+                        Hi! 👋 I'm <span className="text-transparent sm:text-6xl bg-clip-text bg-gradient-to-r from-indigo-700 via-fuchsia-300 to-cyan-400">Roberto</span>.
+                        </h1>
+                        <h1 className="text-left text-gray-600 font-semibold opacity-80">engineer - thinker - explorer </h1>
                         <Contacts />
                     </div>
+                    
                 </div>
                 <div className="min-w-1/2 flex flex-col justify-center items-center">
-                    <img className="w-128 h-128 object-cover rounded-full drop-shadow-xl fill-textWhite" src="https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo="/>
+                    <div className="transform hover:scale-[1.01] transition-all rounded-full bg-gradient-to-r p-3 from-indigo-700 via-fuchsia-300 to-cyan-400">
+                        <div className="transform hover:scale-[1.01] transition-all rounded-full p-3 bg-black">
+                            <img className="rounded-full bg-gradient-to-r w-128 h-128 object-cover" src={pfp} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
