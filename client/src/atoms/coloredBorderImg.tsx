66902c31642ebe0ef27d674d4c725cf7c07f2058
Pochetes <robertomiguel2001@gmail.com>
2022-02-11 22:40:20 -0500

    added imageSize prop for gradient border component

--- client/src/atoms/coloredBorderImg.tsx
+++ client/src/atoms/coloredBorderImg.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ColoredBorderImg = ({ width, height, position, fromColor, viaColor, toColor, mode, image }) => (
+const ColoredBorderImg = ({ width, height, position, fromColor, viaColor, toColor, mode, image, imageSize }) => (
     <div className={`flex flex-col justify-center items-center`}>
         <div className={`${position} ${fromColor} ${viaColor} ${toColor} transform hover:scale-[1.01] transition-all rounded-full bg-gradient-to-r p-3`}>
             <div className={`${mode} transform hover:scale-[1.01] transition-all rounded-full p-3`}>
-                <img className={`${width} ${height} rounded-full object-cover`} src={image} />
+                <img className={`${imageSize} ${width} ${height} rounded-full object-cover`} src={image} />
             </div>
         </div>
     </div>
