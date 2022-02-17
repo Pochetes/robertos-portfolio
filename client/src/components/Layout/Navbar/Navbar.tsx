ec7a7ec7f76605137f31cfca83ee74c5b1404fad
Pochetes <robertomiguel2001@gmail.com>
2022-02-17 06:46:38 -0500

    added logic to mobile menu

---
+++ client/src/components/Layout/Navbar/Navbar.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { SunIcon, MoonIcon, MenuIcon, XIcon } from '@heroicons/react/solid'
+import Hamburger from './Hamburger';
+
+const Navbar = () => {
+    const [hamburgerMenu, setHamburgerMenu] = useState<boolean>(false);
+
+    const handleMenuToggle = () => {
+        setHamburgerMenu(prev => !prev)
+    }
+
+    return (
+        <header className="py-6 px-6 sticky top-0 z-50">
+            <div>
+                <div className="flex justify-between font-semibold">
+                    <div className="lg:hidden">
+                        <button 
+                        type="button" 
+                        onClick={handleMenuToggle}
+                        aria-controls="mobile-menu"
+                        >
+                            {
+                                hamburgerMenu
+                                ? <XIcon className="h-8"/>
+                                : <MenuIcon className="h-8"/>
+                            }
+                        </button>
+                    </div>
+                    <div className="p-2">
+                        <a href="#">Roberto</a>
+                    </div>
+                    <nav className="hidden lg:flex lg:justify-between lg:text-shadow-main lg:space-x-10">
+                        <a className="p-2" href="#">Home</a>
+                        <a className="p-2" href="#">About</a>
+                        <a className="p-2" href="#">Projects</a>
+                        <a className="p-2" href="#">Resume</a>
+                        <a className="p-2" href="#">Extras</a>
+                        <a className="p-2" href="#">Contacts</a>
+                    </nav>
+                    <div>
+                        {/* <SunIcon className="h-8"/> */}
+                        <MoonIcon className="h-8"/>
+                    </div>
+                </div>
+            </div>
+            <Hamburger hamburgerMenu={hamburgerMenu}/>
+        </header>
+    )
+};
+
+export default Navbar;
