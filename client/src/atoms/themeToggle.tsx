c4584dbe7fc79554794b5cdb85cc2ce90d90526f
Pochetes <robertomiguel2001@gmail.com>
2022-02-21 04:51:09 -0500

    added shared state for theme toggle

---
+++ client/src/atoms/themeToggle.tsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import { SunIcon, MoonIcon } from '@heroicons/react/solid'
+import ThemeContext from '../contexts/themeContext'
+
+const ThemeToggle = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext)
+
+    return (
+        <button
+        type="button"
+        // @ts-ignore
+        onClick={toggleTheme}
+        >
+            {
+                theme === "dark" ? 
+                <MoonIcon className="h-8"/>
+                : <SunIcon className="h-8"/>
+            }
+        </button>
+    )
+}
+
+export default ThemeToggle
