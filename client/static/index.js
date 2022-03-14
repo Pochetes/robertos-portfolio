2e7e46a1b651363ff8e18ec5bed17d73e71ed417
Pochetes <robertomiguel2001@gmail.com>
2022-03-13 20:54:51 -0400

    final adjusting of interest images

--- client/static/index.js
+++ client/static/index.js
@@ -4,17 +4,11 @@ import twitter from './social/twitter.svg'
 import pfp from './pfp.jpeg'
 import algo from './crypto/algo.svg'
 import eth from './crypto/eth.svg'
-import sol from './crypto/sol.svg'
 import xlm from './crypto/xlm.svg'
-import lightchip from './interests/chip/lightchip.png'
-import darkchip from './interests/chip/darkchip.png'
-import lightplane from './interests/plane/lightplane.png'
-import darkplane from './interests/plane/darkplane.png'
-import lightsol from './interests/sol/lightsol.png'
-import darksol from './interests/sol/darksol.png'
-import lightspades from './interests/spades/lightspades.png'
-import darkspades from './interests/spades/darkspades.png'
-import app from './interests/chip/app.png'
+import chip from './interests/chip.png'
+import plane from './interests/plane.png'
+import sol from './interests/sol.png'
+import spades from './interests/spades.png'
 
 export {
     github,
@@ -23,15 +17,9 @@ export {
     pfp,
     algo,
     eth,
-    sol,
     xlm,
-    lightchip,
-    darkchip,
-    lightplane,
-    darkplane,
-    lightsol,
-    darksol,
-    lightspades,
-    darkspades,
-    app
+    chip,
+    plane,
+    sol,
+    spades,
 }
