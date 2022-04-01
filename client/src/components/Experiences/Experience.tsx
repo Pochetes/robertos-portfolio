0417640144131893b4be3bfb87d24000a1d91dcc
Pochetes <robertomiguel2001@gmail.com>
2022-04-01 12:57:01 -0400

    added theme logic for iconStyle attribute in Experience component - missing final coloring

--- client/src/components/Experiences/Experience.tsx
+++ client/src/components/Experiences/Experience.tsx
@@ -4,7 +4,7 @@ import 'react-vertical-timeline-component/style.min.css';
 import ThemeContext from '../../contexts/themeContext';
 
 
-const Experience = ({ children, icon, iconColor, date }) => {
+const Experience = ({ children, icon, darkIconColor, lightIconColor, date }) => {
     const { theme } = useContext(ThemeContext);
     const darkColor = "#171717" // slate-800
     const lightColor = "#f3f4f6" // gray-100
@@ -15,7 +15,7 @@ const Experience = ({ children, icon, iconColor, date }) => {
             contentStyle={
                 theme === 'dark' ? 
                 { background: darkColor, color: lightColor, borderRadius: '20px' } 
-                : { background: lightColor, color: darkColor, borderRadius: '20px' }
+                : { background: "#f9fafb", color: darkColor, borderRadius: '20px' }
             }
             contentArrowStyle={
                 theme === 'dark' ?
@@ -23,7 +23,11 @@ const Experience = ({ children, icon, iconColor, date }) => {
                 : { borderRight: `7px solid ${lightColor}` }
             }
             date={date}
-            iconStyle={{ background: iconColor, color: '#fff' }}
+            iconStyle={
+                theme === 'dark' ?
+                { background: darkIconColor, color: '#fff' }
+                : { background: lightIconColor, color: '#fff' }
+            }
             icon={icon}
         >
             {children}
--- client/src/components/Experiences/Experiences.tsx
+++ client/src/components/Experiences/Experiences.tsx
@@ -11,65 +11,85 @@ const Experiences = () => {
         <section>
             <h1 className="md:mt-10 text-5xl text-center md:text-7xl lg:mb-10 text-shadow-main">My Experiences</h1>
             <VerticalTimeline lineColor={theme === 'dark' ? '#f3f4f6' : '#171717'}>
+                {/* First Experience */}
                 <Experience 
                 icon={<GlobeIcon />}
-                iconColor="rgb(233, 30, 99)"
+                darkIconColor="#80ffdb"
+                lightIconColor="#ffba08"
                 date="2012 - present"
                 >
                     <h3 className="text-semibold text-2xl">Liberty Mutual Insurance</h3>
                     <h5 className="mt-1 text-gray-500">Software Engineer Intern</h5>
                 </Experience>
+                {/* Second Experience */}
                 <Experience
                 icon={<GlobeIcon />}
-                iconColor="rgb(233, 30, 99)"
+                darkIconColor="#64dfdf"
+                lightIconColor="#faa307"
                 date="2012 - present"
                 >
                     <h3 className="text-semibold text-2xl">Liberty Mutual Insurance</h3>
                     <h5 className="mt-1 text-gray-500">Software Engineer Intern</h5>
                 </Experience>
+                {/* Third Experience */}
                 <Experience 
                 icon={<GlobeIcon />}
-                iconColor="rgb(233, 30, 99)"
+                darkIconColor="#56cfe1"
+                lightIconColor="#f48c06"
                 date="2012 - present"
                 >
                     <h3 className="text-semibold text-2xl">Liberty Mutual Insurance</h3>
                     <h5 className="mt-1 text-gray-500">Software Engineer Intern</h5>
                 </Experience>
+                {/* Fourth Experience */}
                 <Experience 
                 icon={<GlobeIcon />}
-                iconColor="rgb(233, 30, 99)"
+                darkIconColor="#4ea8de"
+                lightIconColor="#e85d04"
                 date="2012 - present"
                 >
                     <h3 className="text-semibold text-2xl">Liberty Mutual Insurance</h3>
                     <h5 className="mt-1 text-gray-500">Software Engineer Intern</h5>
                 </Experience>
+                {/* Fifth Experience */}
                 <Experience 
                 icon={<GlobeIcon />}
-                iconColor="rgb(233, 30, 99)"
+                darkIconColor="#56cfe1"
+                lightIconColor="#dc2f02"
                 date="2012 - present"
                 >
                     <h3 className="text-semibold text-2xl">Liberty Mutual Insurance</h3>
                     <h5 className="mt-1 text-gray-500">Software Engineer Intern</h5>
                 </Experience>
+                {/* Sixth Experience */}
                 <Experience 
                 icon={<GlobeIcon />}
-                iconColor="rgb(233, 30, 99)"
+                darkIconColor="#5390d9"
+                lightIconColor="#d00000"
                 date="2012 - present"
                 >
                     <h3 className="text-semibold text-2xl">Liberty Mutual Insurance</h3>
                     <h5 className="mt-1 text-gray-500">Software Engineer Intern</h5>
                 </Experience>
+                {/* Seventh Experience */}
                 <Experience 
                 icon={<GlobeIcon />}
-                iconColor="rgb(233, 30, 99)"
+                darkIconColor="#6930c3"
+                lightIconColor="#9d0208"
                 date="2012 - present"
                 >
                     <h3 className="text-semibold text-2xl">Liberty Mutual Insurance</h3>
                     <h5 className="mt-1 text-gray-500">Software Engineer Intern</h5>
                 </Experience>
+                {/* Inception Element */}
                 <VerticalTimelineElement
-                    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                    iconStyle={
+                        theme === 'dark' ?
+                        { background: '#7400b8', color: '#fff' }
+                        : { background: '#370617', color: '#fff' } 
+                    }
                     icon={""}
+                    date="Inception.."
                 />
             </VerticalTimeline>
         </section>
