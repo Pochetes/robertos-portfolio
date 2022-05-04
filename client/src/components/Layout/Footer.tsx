37e6796e840d78dd5c6b01c55e456835e5493f42
Pochetes <robertomiguel2001@gmail.com>
2022-05-04 03:26:38 -0400

    added initial design layout for Footer component

---
+++ client/src/components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const Footer = () => (
+    <footer className="bg-neutral-900 mt-10 h-12 flex justify-center items-end">
+        <p className="text-center mx-5 xs:text-xs xs:mb-3 ">
+            Copyright © 2022 Roberto. All rights reserved.
+        </p>
+    </footer>
+)
+
+export default Footer
--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import Navbar from "./Navbar/Navbar"
-import CopyRight from "./Copyright"
+import Footer from "./Footer"
 import ThemeContext from "../../contexts/themeContext"
 import useDarkMode from "../../hooks/useDarkMode"
 
@@ -19,7 +19,7 @@ const Layout = ({ children, className = "" }: LayoutProps) => {
                     <div className="min-h-screen min-w-screen overflow-auto font-sans font-bold bg-gray-100 text-slate-800 dark:bg-black dark:text-gray-100 transition-colors duration-300">
                         <Navbar />
                         <main className={className}>{children}</main>
-                        <CopyRight />
+                        <Footer />
                     </div>
                 </div>
             </ThemeContext.Provider>
