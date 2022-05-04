b3fe6099a2f630593139ab763a6181965379e243
Pochetes <robertomiguel2001@gmail.com>
2022-05-04 03:25:32 -0400

    added initial design layout for ContactPage component

--- client/src/components/ContactPage/Contact.tsx
+++ client/src/components/ContactPage/Contact.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Contact = ({ image, social }) => (
-    <div className="flex flex-row flex-auto basis-1/4">
-        <div className="basis-1/4">
-            <img src={image} alt="picture of social media" />
-        </div>
-        <h2 className="basis-1/4">{social}</h2>
+const Contact = ({ image, social, link }) => (
+    <div className="bg-gray-50 dark:bg-neutral-900 rounded-3xl xs:px-2 xs:py-3">
+        <a 
+        href={link === "Email" ? `mailto: ${link}` : link}
+        className="xs:flex xs:items-center">
+
+            <img className="w-16 h-16 rounded-full" src={image} alt="social media image" />
+            <h5 className="xs:ml-5 xs:text-4xl">{social}</h5>
+        </a>
     </div>
 )
 
--- client/src/components/ContactPage/ContactPage.tsx
+++ client/src/components/ContactPage/ContactPage.tsx
@@ -1,24 +1,58 @@
 import * as React from "react"
+import { graphql, useStaticQuery } from 'gatsby'
 import Contact from "./Contact"
 
-const ContactPage = () => (
-    <section>
-        <div className="flex flex-row flex-auto">
-            <Contact 
-                image="../../../static"
-                social="LinkedIn"
-            />
-            <Contact 
-                image="../../../static"
-                social="Github"
-            />
-            <Contact 
-                image="../../../static"
-                social="Twitter"
-            />
-        </div>
-        <footer>I'm a footer</footer>
-    </section>
-)
+const ContactPage = () => {
+    // queries svg and functionality for lazy-loading
+    const imageQuery = useStaticQuery(graphql`
+        query {
+            githubImg: file(relativePath: {eq: "social/github.svg"}) {
+                publicURL
+            }
+            linkedinImg: file(relativePath: {eq: "social/linkedin.svg"}) {
+                publicURL
+            }
+            twitterImg: file(relativePath: {eq: "social/twitter.svg"}) {
+                publicURL
+            }
+            emailImg: file(relativePath: {eq: "social/email.svg"}) {
+                publicURL
+            }
+        }
+    `)
+
+    const githubImg = imageQuery.githubImg
+    const linkedinImg = imageQuery.linkedinImg
+    const twitterImg = imageQuery.twitterImg
+    const emailImg = imageQuery.emailImg
+
+    return (
+        <section className="xs:space-y-8">
+            <h1 className="mt-10 text-5xl text-center md:text-7xl lg:mb-10 text-shadow-main" >Let's Talk!</h1>
+            <div className="flex flex-col xs:space-y-8 xs:mx-5">
+                <Contact 
+                    image={githubImg.publicURL}
+                    social="Github"
+                    link="https://www.github.com/Pochetes"
+                />
+                <Contact 
+                    image={linkedinImg.publicURL}
+                    social="LinkedIn"
+                    link="https://www.linkedin.com/in/RobertoMartinez21"
+                />
+                <Contact 
+                    image={twitterImg.publicURL}
+                    social="Twitter"
+                    link="https://www.twitter.com/iamrobmart"
+                />
+                <Contact 
+                    image={emailImg.publicURL}
+                    social="Email"
+                    link="rmart482@fiu.edu"
+                />
+            </div>
+        </section>
+    )
+}
 
 export default ContactPage
