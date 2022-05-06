1e8a7dc5f80cf5d4baa447993ba2f2f5607a8831
Pochetes <robertomiguel2001@gmail.com>
2022-05-05 22:43:17 -0400

    implemented ContactPage component for all screen sizes

--- client/src/components/ContactPage/Contact.tsx
+++ client/src/components/ContactPage/Contact.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Contact = ({ image, social, link }) => (
-    <div className="bg-gray-50 dark:bg-neutral-900 rounded-3xl xs:px-2 xs:py-3">
+    <div className="bg-gray-50 dark:bg-neutral-900 rounded-3xl xs:px-2 xs:py-3 sm:px-3 sm:py-4">
         <a 
         href={link === "Email" ? `mailto: ${link}` : link}
         className="xs:flex xs:items-center">
 
-            <img className="w-16 h-16 rounded-full" src={image} alt="social media image" />
-            <h5 className="xs:ml-5 xs:text-4xl">{social}</h5>
+            <img className="w-16 h-16 sm:w-20 sm:h-20 lg:w-24 lg:h-24 xl:w-32 xl:h-32 rounded-full" src={image} alt="social media image" />
+            <h5 className="xs:ml-5 xs:text-3xl sm:text-4xl lg:text-5xl xl:text-6xl">{social}</h5>
         </a>
     </div>
 )
--- client/src/components/Extras/Extras.tsx
+++ client/src/components/Extras/Extras.tsx
@@ -5,7 +5,7 @@ import Albums from "./Albums"
 
 const Extras = () => (
     <section className="space-y-32">
-        <h1 className="mt-10 text-5xl text-center md:text-7xl lg:mb-10 text-shadow-main" >here is ... cool stuff</h1>
+        <h1 className="mt-32 text-5xl text-center md:text-7xl lg:mb-10 text-shadow-main" >here is ... cool stuff</h1>
         <Contributions />
         <Podcasts />
         <Albums />
