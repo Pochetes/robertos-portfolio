aa72137b5b38e49d0c85f808c2ab14493ecf73f2
Pochetes <robertomiguel2001@gmail.com>
2022-05-07 05:34:50 -0400

    implemented scroll to section with refs in footer component

--- client/src/components/Layout/Footer.tsx
+++ client/src/components/Layout/Footer.tsx
@@ -1,19 +1,34 @@
-import * as React from "react"
+import React, { useContext } from "react"
 import Navlinks from "./Navbar/Navlinks"
+import NavContext from "../../contexts/NavContext"
 
-const Footer = () => (
-    <footer className="bg-gray-200 dark:bg-neutral-900 mt-48 h-fit flex flex-col md:mt-64">
-        <div className="flex flex-col space-y-8 mt-10 ml-8 text-lg text-slate-600 dark:text-gray-300 md:text-2xl md:space-y-12 md:mt-14 md:ml-14">
-            {Navlinks.map((item) => (
-                <a key={item.id} href={item.href} className="p-2 xs:w-28 lg:gradient-link">
-                {item.name}
-                </a>
-            ))}
-        </div>
-        <p className="text-center text-slate-600 xs:text-sm xs:my-6 sm:text-lg sm:my-8">
-            Copyright © 2022 ❤️ Roberto. All rights reserved.
-        </p>
-    </footer>
-)
+const Footer = () => {
+    const { navRefs } = useContext(NavContext)
 
+    const scrollToSection = (index) => {
+        window.scrollTo({
+            top: navRefs[index].current.offsetTop,
+            behavior: "smooth"
+        })
+    }
+
+    return (
+        <footer className="bg-gray-200 dark:bg-neutral-900 mt-48 h-fit flex flex-col md:mt-64">
+            <div className="flex flex-col space-y-8 mt-10 ml-8 text-lg text-slate-600 dark:text-gray-300 md:text-2xl md:space-y-12 md:mt-14 md:ml-14">
+                {Navlinks.map((item) => (
+                    <div 
+                    key={item.id}
+                    onClick={() => scrollToSection(item.id)}
+                    className="p-2 xs:w-28 lg:gradient-link"
+                    >
+                    {item.name}
+                    </div>
+                ))}
+            </div>
+            <p className="text-center text-slate-600 xs:text-sm xs:my-6 sm:text-lg sm:my-8">
+                Copyright © 2022 ❤️ Roberto. All rights reserved.
+            </p>
+        </footer>
+    )
+}
 export default Footer
