48c4360a4e02abf5d56962e02d33692b11acd1e8
Pochetes <robertomiguel2001@gmail.com>
2022-05-11 05:50:10 -0400

    filled in placeholders in Projects section - added project imgs

--- client/src/components/Projects/Project/Tag.tsx
+++ client/src/components/Projects/Project/Tag.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Tag = ({ text }) => (
-    <li
+    <div
     className="inline-block px-3 py-1 md:px-4 md:py-1.5 text-xs font-semibold bg-neutral-800 shadow-sm dark:shadow-xl shadow-neutral-900/50 text-gray-100 rounded-full"
     >
         {text}
-    </li>
+    </div>
 )
 
 export default Tag
--- client/src/components/Projects/Slides.tsx
+++ client/src/components/Projects/Slides.tsx
@@ -2,30 +2,36 @@ const Slides = [
     {
         id: 0,
         title: "Navi Web Companion",
-        description: "Some Google Chrome Extension",
-        image: "https://www.hyperui.dev/photos/team-1.jpeg",
-        link: "https://github.com/Pochetes/repo1",
+        description: "Google Chrome extension that uses Natural Language Processing to\
+        summarize highlighted text. Never waste time reading again.",
+        image: "../../images/projects/naviWebCompanion.png",
+        techUsed: ["React", "Chrome Extensions API", "TextRank"],
+        link: "https://github.com/Pochetes/Navi-Web-Companion",
     },
     {
         id: 1,
         title: "SmartClaims",
-        description: "Some Blockchain application for insurance claims",
-        image: "https://www.hyperui.dev/photos/team-1.jpeg",
-        link: "https://github.com/Pochetes/repo2",
+        description: "A blockchain-powered Application that processes commercial insurance claims\
+        in the Property & Casualty, Auto, and Health sectors for Liberty Mutual.",
+        image: "../../images/projects/smartClaims.png",
+        techUsed: ["React", "Solidity", "Truffle", "Metamask", "Web3.js"],
+        link: "https://github.com/Pochetes/SmartClaims-Blockchain",
     },
     {
         id: 2,
         title: "Portfolio API",
-        description: "Some API I made for my portfolio website",
-        image: "https://www.hyperui.dev/photos/team-1.jpeg",
-        link: "https://github.com/Pochetes/repo3",
+        description: "The RESTful web service that powers the data in my portfolio website.",
+        image: "../../images/projects/portfolioApi.png",
+        techUsed: ["Python", "MongoDB", "Docker", "REST APIs"],
+        link: "https://github.com/Pochetes/portfolio-api",
     },
     {
         id: 3,
         title: "RealTalk Application",
-        description: "Some messaging application using socket programming principles",
-        image: "https://www.hyperui.dev/photos/team-1.jpeg",
-        link: "https://github.com/Pochetes/repo4",
+        description: "A scalable, socket-induced, live chat interface that uses low latency threading to speak to other people connected.",
+        image: "../../images/projects/realTalkApp.png",
+        techUsed: ["NodeJS", "Express", "HTML/CSS", "Websockets"],
+        link: "https://github.com/Pochetes/RealTalk-App",
     },
 ]
 
