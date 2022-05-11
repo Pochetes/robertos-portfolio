9496b601a817eca74783d67498ae7be4a9e4b2a0
Pochetes <robertomiguel2001@gmail.com>
2022-05-11 00:04:47 -0400

    fixed skill object sizing for all screen widths

--- client/src/components/Skills/Skill.tsx
+++ client/src/components/Skills/Skill.tsx
@@ -6,7 +6,7 @@ const Skill = ({ image }) => {
     <div className="shadow-xl rounded-2xl xs:mb-5 md:mb-0">
       <a className="block bg-gray-50 dark:bg-neutral-900 rounded-xl">
         <div className="mt-4 xs:p-4 sm:p-7 flex flex-col justify-center content-center">
-          <GatsbyImage className="lg:grayscale lg:hover:grayscale-0 lg:transition lg:ease-in-out lg:delay-150" image={image} alt="skill" />
+          <GatsbyImage className="lg:grayscale lg:hover:grayscale-0 lg:transition lg:ease-in-out lg:delay-75" image={image} alt="skill" />
         </div>
       </a>
     </div>
--- client/src/components/Skills/Skills.tsx
+++ client/src/components/Skills/Skills.tsx
@@ -25,9 +25,9 @@ const Skills = () => {
 
     return (
         <section id="skills-header">
-            <div className="xs:mx-10 md:mx-14">
+            <div className="">
                 <h1 className="sm:mt-14 md:mt-32 text-5xl text-center md:text-7xl lg:mb-10 text-shadow-main">My Skills</h1>
-                <div className="md:grid md:grid-cols-3 lg:grid-cols-4 md:gap-5 xs:mt-14">
+                <div className="md:grid md:grid-cols-3 lg:grid-cols-4 md:gap-5 xs:mt-14 xs:mx-10 s2:mx-16 sm:mx-32 md:mx-14 lg:mx-32">
                     <Skill image={metaImg} />
                     <Skill image={metaImg} />
                     <Skill image={metaImg} />
