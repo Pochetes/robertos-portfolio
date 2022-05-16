057156f6fd77720ce19c7287bee152d0ced64456
Pochetes <robertomiguel2001@gmail.com>
2022-05-16 05:30:27 -0400

    fixing mobile to desktop project component

--- client/src/components/Projects/Project/Project.tsx
+++ client/src/components/Projects/Project/Project.tsx
@@ -8,30 +8,37 @@ const Project = ({ title, description, image, techUsed, link }) => {
     const isMobile = useMediaQuery({ query: `(max-width: 640px)` });
 
     return (
-        <div className={`${isMobile ? "xs:w-64 s1:w-72 s2:w-80 sm:w-80" : ""} bg-white dark:bg-neutral-900 shadow-lg dark:shadow-neutral-800/50 rounded-xl`}>
-            <GatsbyImage className="object-cover w-full h-80 rounded-t-lg" image={image} alt="" />
-            <div className="p-4">
-                {/* project title */}
-                <h1 className="text-xl sm:text-2xl md:text-3xl font-bold">
-                {title}
-                </h1>
+        <div className={`${ isMobile ? "xs:w-64 s1:w-72 s2:w-80 sm:w-80" : ""} shadow-xl dark:shadow-xl dark:shadow-neutral-800/50 rounded-2xl`}>
+            <div className="block bg-gray-50 dark:bg-neutral-900 rounded-xl">
+                <GatsbyImage
+                    className="object-cover h-64 rounded-t-lg"
+                    image={image}
+                    alt="project image"
+                />
 
-                {/* description */}
-                <p className="mt-2 text-sm md:text-md lg:text-lg text-gray-600">
-                {description}
-                </p>
+                <div className="p-4">
+                    {/* project title */}
+                    <h5 className="text-xl sm:text-2xl md:text-3xl font-bold">
+                    {title}
+                    </h5>
 
-                {/* tags */}
-                <div className="mt-5 flex flex-wrap">
-                    {techUsed.map((tech, index) => {
-                        return <Tag key={index} text={tech}/>
-                    })}
+                    {/* description */}
+                    <p className="mt-2 text-sm md:text-md text-gray-600">
+                    {description}
+                    </p>
+
+                    {/* tags */}
+                    <div className="mt-5 flex flex-wrap">
+                        {techUsed.map((tech, index) => {
+                            return <Tag key={index} text={tech}/>
+                        })}
+                    </div>
+                    
+                    {/* Link to project */}
+                    <a href={link}>
+                        <Link />
+                    </a>
                 </div>
-                
-                {/* Link to project */}
-                <a href={link}>
-                    <Link />
-                </a>
             </div>
         </div>
     )
--- client/src/components/Projects/Projects.tsx
+++ client/src/components/Projects/Projects.tsx
@@ -1,15 +1,23 @@
-import * as React from "react"
+import React, { useState } from "react"
 import Carousel from "./Carousel"
 import TouchSlider from "./TouchSlider"
 import { useMediaQuery } from 'react-responsive'
+import { useEffect } from "react"
 
 const Projects = () => {
-    const isMobile = useMediaQuery({ query: `(max-width: 640px)` });
+    const [mobile, setMobile] = useState(false)
+    const mediaQuery = useMediaQuery({ query: `(max-width: 640px)` });
+
+    useEffect(() => {
+        if(mediaQuery !== mobile) {
+            setMobile(mediaQuery)
+        }
+    }, [mediaQuery])
 
     return (
         <section id="projects-header" className="xs:mb-20">
             <h1 className="md:mt-10 text-5xl text-center md:text-7xl text-shadow-main">Projects</h1>
-            {isMobile ? <TouchSlider /> : <Carousel />}
+            {!mobile ? <Carousel /> : <TouchSlider />}
         </section>
     )
 }
